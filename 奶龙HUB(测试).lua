3a46efde16d7bce2e67974ad06c84f95322ae8aa
zczczczc766 <2674134570@qq.com>
2026-09-12 08:41:42 +0800

    Add files via upload

--- 奶龙HUB(测试).lua
+++ 奶龙HUB(测试).lua
@@ -1480,7 +1480,7 @@ local O=D:Tab({Title="被遗弃",Icon="ghost"})
 O:Button({
     Title = "加载角色/皮肤修改器",
     Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/zczczczc766/ink/refs/heads/main/%E8%A2%AB%E9%81%97%E5%BC%83%E8%A7%92%E8%89%B2or%E7%9A%AE%E8%82%A4%E5%88%87%E6%8D%A2%E5%99%A8.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/zczczczc766/NailongHUB/refs/heads/main/%E8%A2%AB%E9%81%97%E5%BC%83%E8%A7%92%E8%89%B2or%E7%9A%AE%E8%82%A4%E5%88%87%E6%8D%A2%E5%99%A8.lua"))()
     end
 })
 
